eecc29d2ca758460a30a74444238334b12b7dd47
Ulises Urbina <ulises.urbina@eossoftware.mx>
2025-05-22 17:19:20 -0600

    UPD countdown

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,6 @@
 import RotatingText from './RotateText'
+import Countdown from '../utils/Countdown';
+
 import '../App.css'
 import '../styles/RotatingText.css'
 import hero from '../assets/hero.png'
@@ -25,9 +27,10 @@ function Banner() {
           {/* <h2>Araceli & Alan</h2> */}
           <h3>18 Octubre 2025</h3>
           <div className='ContenidoBanner_texto'>
-            <h3>¡Bienvenidos a nuestra historia de amor! ♥</h3>
-            <h4>Nos emociona compartir con ustedes este momento tan especial. <br /> Por favor confirma tu asistencia lo antes posible, <br /> así sabremos si contamos contigo para celebrar juntos. <br /> ¡Disfruta el sitio y esperamos verte muy pronto!</h4>
+            <h3>¡Bienvenidos a nuestra historia de amor! <span className="Heart">♥</span></h3>
+            <h4>Nos emociona compartir con ustedes este momento tan especial. <br /> Por favor confirma tu asistencia lo antes posible, <br /> así sabremos si contamos contigo para celebrar juntos. <br /> Disfruta el sitio y esperamos verte dentro de:</h4>
           </div>
+          <Countdown />
         </div>
       </div>
     </section>
---
+++ src/utils/Countdown.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/Countdown.css';
+
+const Countdown = () => {
+  const countDownDate = new Date("Oct 18, 2025 10:00:00").getTime();
+
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      if (distance < 0) {
+        clearInterval(interval);
+        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        setTimeLeft({
+          days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+          minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((distance % (1000 * 60)) / 1000)
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  return (
+    <div className="CountdownContainer">
+        <div>
+            <h4>{timeLeft.days}</h4>
+            <h5 id="relojDias">Días</h5>
+        </div>
+        <span >:</span>
+        <div>
+            <h4>{timeLeft.hours}</h4>
+            <h5>Horas</h5>
+        </div>
+        <span >:</span>
+        <div>
+            <h4>{timeLeft.minutes}</h4>
+            <h5 id="relojMinutos">Minutos</h5>
+        </div>
+        <span >:</span>
+        <div>
+            <h4>{timeLeft.seconds}</h4>
+            <h5 id="relojSegundos">Segundos</h5>
+        </div>
+    </div>
+  );
+};
+
+export default Countdown;
